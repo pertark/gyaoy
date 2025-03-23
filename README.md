7e72da668cd28265d0b6f516fec43f6692e40344
Patrick Zhang <zhangpatrick2004@gmail.com>
2025-03-23 23:41:31 +0800

    add support for other websites

--- README.md
+++ README.md
@@ -5,5 +5,10 @@ problem: habit of opening youtube
 solution: be greeted with this when you open youtube ![example image](assets/example.png)  
 
 ## extra features
+ - Add any website to the blacklist
  - Discord webhook integration to ping you in shame
  - Send a mocking image
+
+## how to use
+ - Install Chrome extension
+ - Configure options in extension "Options" page (right click extension icon)
--- options.js
+++ options.js
@@ -3,17 +3,35 @@ document.getElementById('saveButton').addEventListener('click', function() {
   const username = document.getElementById('username').value;
   const uid = document.getElementById('uid').value;
 
-  chrome.storage.sync.set({ webhookUrl: webhookUrl, username: username, uid: uid }, function() {
+	// note: url validation more annoying than worth
+	const bannedSites = [...document.getElementById('banned-websites').children]
+		.map((node) => node.value)
+		.filter((node) => node);
+	console.log(bannedSites);
+
+  chrome.storage.sync.set({ webhookUrl: webhookUrl, username: username, uid: uid, bannedSites: bannedSites }, function() {
+		let msg = '';
     if (uid) {
-      document.getElementById('status').textContent = 'Webhook URL, Username and UID saved.';
+      msg = 'Webhook URL, Username and UID saved.';
     } else {
-      document.getElementById('status').textContent = 'Webhook URL and Username saved.';
+      msg = 'Webhook URL and Username saved.';
     }
+		if (bannedSites.length) {
+      msg += '\nBanned sites saved.'
+		} 
+		document.getElementById('status').textContent = msg;
   });
 });
 
+document.getElementById("site-add").addEventListener("click", function() {
+	let node = document.createElement('input');
+	node.type = 'text';
+	node.value = "";
+	document.getElementById('banned-websites').appendChild(node);
+})
+
 document.addEventListener('DOMContentLoaded', function() {
-  chrome.storage.sync.get(['webhookUrl', 'username', 'uid'], function(data) {
+  chrome.storage.sync.get(['webhookUrl', 'username', 'uid', 'bannedSites'], function(data) {
     if (data.webhookUrl) {
       document.getElementById('webhookUrl').value = data.webhookUrl;
     }
@@ -23,5 +41,25 @@ document.addEventListener('DOMContentLoaded', function() {
     if (data.uid) {
       document.getElementById('uid').value = data.uid;
     }
+		if (!data.bannedSites) {
+			// default sites, first init.
+			let node = document.createElement('input');
+			node.type = 'text';
+			node.value = "youtube.com";
+			document.getElementById('banned-websites').appendChild(node);
+			let node2 = document.createElement('input');
+			node2.type = 'text';
+			node2.value = "www.youtube.com";
+			document.getElementById('banned-websites').appendChild(node2);
+			chrome.storage.sync.set({ bannedSites: ['youtube.com', 'www.youtube.com'] })
+		}
+		else if (data.bannedSites && data.bannedSites.length > 0) {
+			for (const site of data.bannedSites) {
+				let node = document.createElement('input');
+				node.type = 'text';
+				node.value = site;
+				document.getElementById('banned-websites').appendChild(node);
+			}
+		}
   });
 });
